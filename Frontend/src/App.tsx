882e79a2903ae16ec65980362e1d201054977636
Elliott DuCharme <ellduc4@gmail.com>
2025-03-30 13:45:48 -0500

    Create Starter React/Vite project and add to Docker Compose

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Home from "./Home.tsx";
+
+export default function App() {
+    return (
+        <div className="h-full w-full">
+            <Home/>
+        </div>
+    );
+}
---
+++ Frontend/src/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    function onClick() {
+    }
+
+    return (
+        <div className="">
+            <button onClick={onClick}>
+                hi
+            </button>
+        </div>
+    );
+}
